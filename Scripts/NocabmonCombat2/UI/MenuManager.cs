e75a0ed05ad300e5a93775055956eac3a3ac7e2c
toon-leader-bacon <bacon247@gmail.com>
2023-08-26 18:06:57 -0400

    Added menu script

--- Scripts/NocabmonCombat2/UI/MenuManager.cs
+++ Scripts/NocabmonCombat2/UI/MenuManager.cs
@@ -11,7 +11,15 @@ public class MenuManager : MonoBehaviour
     public Button itemButton;
     public Button runButton;
 
-    protected List<Button> menuButtons = new List<Button>(4);
+    /**
+     * 2d array of buttons. The buttons are in the list to mirror how they appear on the screen,
+     * with the (x,y) coordinates corresponding to the array indexes
+     * So menuButtons[0][0]; is the top left button while menuButtons[1][1] is the bot right.
+     * In other words, this is a list of columns. The first index (x) specifies which column to select
+     * while the second index (y) selects a row.
+     */
+    protected List<List<Button>> menuButtons = new List<List<Button>>(); // 2d array of buttons
+
     protected Vector2Int currentSelected = new Vector2Int(0, 0); // 0,0 is top left of the button array
 
     // Start is called before the first frame update
@@ -24,14 +32,50 @@ public class MenuManager : MonoBehaviour
         runButton.onClick.AddListener(OnRunClick);
 
         // Put buttons into list
-        menuButtons.Add(attackButton); // (0,0)
-        menuButtons.Add(magicButton); // (0,0)
-        menuButtons.Add(itemButton); // (0,0)
-        menuButtons.Add(runButton); // (0,0)
+        List<Button> colX0 = new List<Button>(2);
+        colX0.Add(attackButton); // (0,0) Top Left
+        colX0.Add(magicButton); // (0,1) Bottom Left
+
+        List<Button> colX1 = new List<Button>(2);
+        colX1.Add(itemButton); // (1,0) Top Right
+        colX1.Add(runButton); // (1,1) Bottom Right
+
+        menuButtons.Add(colX0);
+        menuButtons.Add(colX1);
     }
 
     // Update is called once per frame
-    void Update() { }
+    void Update()
+    {
+        Vector2Int menuMoveDir = new Vector2Int(0, 0);
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            menuMoveDir.y += 1;
+        }
+        else if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            menuMoveDir.y -= 1;
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            menuMoveDir.x += 1;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            menuMoveDir.x -= 1;
+        }
+        if (menuMoveDir != new Vector2Int(0, 0))
+        {
+            // Only change highlighting if we move
+            ChangeHighlightedButton(menuMoveDir);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            PressHighlightedButton();
+        }
+    }
 
     // Button click functions
     private void OnAttackClick()
@@ -56,5 +100,28 @@ public class MenuManager : MonoBehaviour
     {
         Debug.Log("Run option selected!");
         // Add your run logic here
+    }
+
+    private void ChangeHighlightedButton(Vector2Int directionMove)
+    {
+        // Deselect the current button
+        menuButtons[currentSelected.x][currentSelected.y].Select();
+
+        // Calculate the newly selected position
+        // [Attack] [Item]
+        // [Magic ] [Run ]
+        int vertMenuLength = 2;
+        int horizMenuLength = 2;
+        currentSelected.x =
+            (currentSelected.x + directionMove.x + horizMenuLength) % horizMenuLength;
+        currentSelected.y = (currentSelected.y + directionMove.y + vertMenuLength) % vertMenuLength;
+
+        // Select the new button
+        menuButtons[currentSelected.x][currentSelected.y].Select();
+    }
+
+    private void PressHighlightedButton()
+    {
+        menuButtons[currentSelected.x][currentSelected.y].onClick.Invoke();
     }
 }
